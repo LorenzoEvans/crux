65e3329d2d93ec04a40e65e16fd7d5cd2bbfcd7e
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-02-03 17:36:19 -0500

    Tweaking some minor mistakes

--- crux-app/src/crux_app/core.clj
+++ crux-app/src/crux_app/core.clj
@@ -11,8 +11,9 @@
   (crux/start-node
     {:crux.node/topology :crux.standalone/topology
      :crux.node/kv-store "crux.kv.membd/kv"
-     :crux.standalone/event-log-dir "data/db-dir"
-     :crux.standalone/event-log-dir "crux.kv.memdb/kv"}))
+     :crux.standalone/event-log-dir "data/eventlog-1"
+     :crux.kv/db-dir "data/db-dir"
+     :crux.standalone/event-log-kv-store "crux.kv.memdb/kv"}))
 
 (def manifest
   {:crux.db/id :manifest
@@ -24,3 +25,23 @@
 
 (crux/submit-tx crux [[:crux.tx/put manifest]])
 (crux/entity (crux/db crux) :manifest)
+
+(crux/submit-tx crux
+  [[:crux.tx/put
+    {:crux.db/id :commodity/Pu
+     :common-name "Plutonium"
+     :type :element/metal
+     :density 19.816
+     :radioactive true}]
+   [:crux.tx/put
+     {:crux.db/id :commodity/N
+      :common-name "Nitrogen"
+      :type :element/gas
+      :density 1.2506
+      :radioactive false}]
+   [:crux.tx/put
+      {:crux.db/id :commodity/CH4
+       :common-name "Methane"
+       :type :molecule/gas
+       :density 0.717
+       :radioactive false}]])
