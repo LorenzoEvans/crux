1cd516e6b7c0d95d4a48a24fc16ff8afab780617
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-02-03 18:59:48 -0500

    Oh boy

--- crux-app/src/crux_app/core.clj
+++ crux-app/src/crux_app/core.clj
@@ -2,27 +2,22 @@
   (:require [crux.api :as crux])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
-
-(def crux
+(defn crux []
   (crux/start-node
     {:crux.node/topology :crux.standalone/topology
      :crux.node/kv-store "crux.kv.membd/kv"
      :crux.standalone/event-log-dir "data/eventlog-1"
      :crux.kv/db-dir "data/db-dir"
      :crux.standalone/event-log-kv-store "crux.kv.memdb/kv"}))
-
-(def manifest
+(crux)
+(defn manifest []
   {:crux.db/id :manifest
    :pilot-name "Lorenzo"
    :id/rocket "SB002-sol"
    :id/employee "22910x2"
    :badges "SETUP"
    :cargo ["stereo" "gold fish" "slippers" "secret note"]})
-
+(manifest)
 (crux/submit-tx crux [[:crux.tx/put manifest]])
 (crux/entity (crux/db crux) :manifest)
 
@@ -45,3 +40,9 @@
        :type :molecule/gas
        :density 0.717
        :radioactive false}]])
+
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
